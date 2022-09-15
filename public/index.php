4b10e887de33f36cd31fe6133dce31c3deddd7da
KaterinaLuuu <80683270+KaterinaLuuu@users.noreply.github.com>
2022-09-15 22:33:26 +0600

    Setup the project (#1)
    * Настройка проекта
    
    * Project setup
    
    * Setup the project
    
    * Setup the project
    
    Co-authored-by: Екатерина Любицкая <ekaterina_l_qschool@qsoft.ru>

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+use App\HelloWorld;
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+$hello = new HelloWorld();
+echo $hello->hello();
---
+++ src/HelloWorld.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+class HelloWorld
+{
+    /**
+     * @return string
+     */
+    public function hello(): string
+    {
+        return 'Hello world!';
+    }
+}
