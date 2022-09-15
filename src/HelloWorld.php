07ffb6cebe29671ce2edb77f088b6483d9b8b65b
катерина юбицкая <ekaterina_l_qschool@qsoft.ru>
2022-09-15 20:51:03 +0500

    Настройка проекта

---
+++ src/HelloWorld.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+class HelloWorld
+{
+    /**
+     * @return void
+     */
+    public function hello(): void
+    {
+        echo 'Hello world!';
+    }
+}
